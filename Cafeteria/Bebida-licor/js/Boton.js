6c4e0ad2d4e542379d6a19ccc0d375257e72d2e7
JustAnotherB00lean <microchip1512@gmail.com>
2016-06-01 15:15:22 -0600

    Update Boton.js

--- Cafeteria/Bebida-licor/js/Boton.js
+++ Cafeteria/Bebida-licor/js/Boton.js
@@ -1,94 +1,32 @@
-var fotoenstring = "";
-
-function insertarBebida()
-{
-    var NuevaBebida = new Object();
-    NuevaBebida.Cod_bebida  = document.getElementById("bebida").value;
-    NuevaBebida.Cod_licor  =  document.getElementById("licor").value;
-    NuevaBebida.op= "Insertar";
-    
-    var fileReader = new FileReader();
- 
-    fileReader.onload = function(fileLoadedEvent) 
-    {
-       
-        fotoenstring = fileLoadedEvent.target.result;
-       
-    };
-    
-    fileReader.readAsDataURL(fileToLoad);
+<script type="text/javascript">
+function listadeproductos() {
+    var Producto = new Object();
+    Producto.op = "listar";
 
     $.ajax({
-        url:"http://localhost:4684/Api/LicorBebida", 
-        type: "POST", 
-        datatype: "json", 
-        data: NuevaBebida, 
-        success:function(data,textstatus,xhr){
-            if(data==1)
-            { 
-                alerta("Correcto Rick") 
-            } 
-        }, 
-        error:function(xhr,textstatus,errorThrown){ 
-            alert(textstatus); } 
-    });
-}
-function ModificarBebida()
-{
-    var ModificarBebida = new Object();
-   NuevaBebida.Cod_bebida  = document.getElementById("Codigo Bebida").value;
-    NuevaBebida.Cod_licor  =  document.getElementById("Codigo Bebida").value;
-    ModificarBebida.op= "Modificar";
-    
-    var fileToLoad = document.getElementById("imagencita").files[0];
-    var fileReader = new FileReader();
- 
-    fileReader.onload = function(fileLoadedEvent) 
-    {
-       
-        fotoenstring = fileLoadedEvent.target.result;
+        url: "http://localhost:1331/api/Producto",
+   
+        type: 'POST',
+        dataType: 'json',
+        data: Producto,
+        success: function (data, textStatus, xhr) {
+            
+            for (var ele in data) {
+                var x = document.getElementById('Bebida')
+                x= x + "<li id='1'> <span>India</span> </li>"
+                var option = document.createElement("option");
+                option.text = data[ele]._Nombre;
+                option.value = data[ele]._Id;
+                option.id= data[ele]._Id;
+                x.add(option);
+            }
 
-    };
-    
-    fileReader.readAsDataURL(fileToLoad);
 
-    ModificarBebida.foto = fotoenstring;
-    $.ajax({
-        url:"http://localhost:4684/Api/Bebidas", 
-        type: "POST", 
-        datatype: "json", 
-        data: ModificarBebida, 
-        success:function(data,textstatus,xhr){
-            if(data==1)
-            { 
-                alerta("Correcto Rick") 
-            } 
-        }, 
-        error:function(xhr,textstatus,errorThrown){ 
-            alert(textstatus); } 
-    });
-}
+        },"
+            error: function (xhr, textStatus, errorThrown) {
+                alert(xhr);
+            }
+});
 
-function EliminarBebida()
-{
-  var EliminarBebida = new Object();
-   NuevaBebida.Cod_bebida  = document.getElementById("Codigo Bebida").value;
-    NuevaBebida.Cod_licor  =  document.getElementById("Codigo Bebida").value;
-    EliminarBebida.Op= "Eliminar";
-    
-    
-    $.ajax({
-        url:"http://localhost:4684/Api/Bebidas", 
-        type: "POST", 
-        datatype: "json", 
-        data: EliminarBebida, 
-        success:function(data,textstatus,xhr){
-            if(data==1)
-            { 
-                alerta("Correcto Rick") 
-            } 
-        }, 
-        error:function(xhr,textstatus,errorThrown){ 
-            alert(textstatus); } 
-    });
 }
+</script>

7c4d8c80176a30782a553bb271b141446c2aa861
JustAnotherB00lean <microchip1512@gmail.com>
2016-06-02 09:37:18 -0600

    Funcion insertar

--- Cafeteria/Bebida-licor/js/Boton.js
+++ Cafeteria/Bebida-licor/js/Boton.js
@@ -26,6 +26,71 @@ function listadeproductos() {
         }
     });
 
+
+    function insertarproducto() {
+        var NuevaCliente = new Object();
+        NuevaCliente.Cod_bebida = document.getElementById("Bebida").value;
+        NuevaCliente.Cod_licor = document.getElementById("Licor").value;
+        NuevaCliente.op = "Insertar";
+        $.ajax({
+            url: "http://localhost:4684/Api/LicorBebida",
+            type: "POST",
+            datatype: "json",
+            data: NuevaCliente,
+            success: function (data, textstatus, xhr) {
+                if (data == 1) {
+                    alerta("Correcto Rick")
+                }
+            },
+            error: function (xhr, textstatus, errorThrown) {
+                alert(textstatus);
+            }
+        });
+    }
+    function ModificarCliente() {
+        var ModificarCliente = new Object();
+        ModificarCliente.id = document.getElementById("id").value;
+        ModificarCliente.nombre = document.getElementById("nombre").value;
+        ModificarCliente.tipo = document.getElementById("tipo").value;
+        ModificarCliente.correo = document.getElementById("correo").value;
+        ModificarCliente.password = document.getElementById("password").value;
+        ModificarCliente.op = "Modificar";
+        $.ajax({
+            url: "http://localhost:4684/Api/Cliente",
+            type: "POST",
+            datatype: "json",
+            data: ModificarCliente,
+            success: function (data, textstatus, xhr) {
+                if (data == 1) {
+                    alerta("Correcto Rick")
+                }
+            },
+            error: function (xhr, textstatus, errorThrown) {
+                alert(textstatus);
+            }
+        });
+    }
+
+    function EliminarCliente() {
+        var EliminarCliente = new Object();
+        EliminarCliente.id = document.getElementById("id").value;
+        EliminarCliente.Op = "Eliminar";
+        $.ajax({
+            url: "http://localhost:4684/Api/Cliente",
+            type: "POST",
+            datatype: "json",
+            data: EliminarCliente,
+            success: function (data, textstatus, xhr) {
+                if (data == 1) {
+                    alerta("Correcto Rick")
+                }
+            },
+            error: function (xhr, textstatus, errorThrown) {
+                alert(textstatus);
+            }
+        });
+    }
+
     //$.ajax({
     //    url: "http://localhost:4684/api/Licor",
 

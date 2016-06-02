ea67ae5d7eba07dccec191500d16feac504aaf62
jorgev259 <jorgev259@outlook.es>
2016-06-02 10:43:25 -0600

    3 drops ajax

--- Cafeteria/Producto-combo/js/Boton.js
+++ Cafeteria/Producto-combo/js/Boton.js
@@ -1,13 +1,17 @@
 var contador_bebida = 0;
-var contador_licor = 0;
+var contador_bocadillo = 0;
+var contador_combo = 0;
 
 function listadeproductos() {
     var bebida = new Object();
-    var licor = new Object();
+    var bocadillo = new Object();
+    var combo = new Object();
     bebida.op = "listar";
-    licor.op = "listar";
+    bocadillo.op = "listar";
+    combo.op = "listar";
     contador_bebida += 1;
-    contador_licor += 1;
+    contador_bocadillo += 1;
+    contador_combo += 1;
 
     $.ajax({
         url: "http://localhost:4684/api/Bebidas",
@@ -27,16 +31,34 @@ function listadeproductos() {
     });
 
     $.ajax({
-        url: "http://localhost:4684/api/Licor",
+        url: "http://localhost:4684/api/Bocadillos",
 
         type: 'POST',
         dataType: 'json',
-        data: licor,
+        data: bocadillo,
         success: function (data, textStatus, xhr) {
 
             for (var ele in data) {
-                var y = document.getElementById('Licor').innerHTML;
-                document.getElementById('Licor').innerHTML = y + "<option value='" + data[ele]._Cod_licor + "' class='" + contador_licor + "'>" + data[ele]._nombre + "</option>";
+                var y = document.getElementById('Bocadillo').innerHTML;
+                document.getElementById('Bocadillo').innerHTML = y + "<option value='" + data[ele]._Cod_bocadillo + "' class='" + contador_bocadillo + "'>" + data[ele]._Nombre + "</option>";
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert(xhr + " la wea fallo");
+        }
+    });
+
+    $.ajax({
+        url: "http://localhost:4684/api/Combos",
+
+        type: 'POST',
+        dataType: 'json',
+        data: combo,
+        success: function (data, textStatus, xhr) {
+
+            for (var ele in data) {
+                var z = document.getElementById('Combo').innerHTML;
+                document.getElementById('Combo').innerHTML = z + "<option value='" + data[ele]._Cod_combo + "' class='" + contador_combo + "'>" + data[ele]._Cod_combo + "</option>";
             }
         },
         error: function (xhr, textStatus, errorThrown) {
@@ -50,6 +72,7 @@ function insertarproducto() {
     var NuevaCliente = new Object();
     NuevaCliente.Cod_bebida = document.getElementById("Bebida").value;
     NuevaCliente.Cod_licor = document.getElementById("Licor").value;
+    NuevaCliente.Cod_licor = document.getElementById("Combo").value;
     NuevaCliente.op = "Insertar";
     $.ajax({
         url: "http://localhost:4684/Api/LicorBebida",
